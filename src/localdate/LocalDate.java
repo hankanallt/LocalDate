81a765acda946e08b7112c8a01f112c6e6dda634
Harkanwal Singh <hankanallt@gmail.com>
2019-11-30 11:03:18 +0100

    First Commit

---
+++ src/localdate/LocalDate.java
@@ -0,0 +1,19 @@
+
+package localdate;
+
+import java.util.Date;
+
+
+public class LocalDate {
+
+    
+    public static void main(String[] args) {
+        
+        Date now;
+        now = new Date();
+        String nowString = now.toString();
+        System.out.println(nowString);
+        
+    }
+    
+}
